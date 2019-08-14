0bad2aa3541c322ddd69bf96d28fe21750697d19
colfin-96 <finger.colin@gmail.com>
2019-08-14 21:40:40 +0200

    Cleaned up code + fixed and removed some old stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
 import comapnyInfo from '../data/companies.json';
 import locationsInfo from '../data/locations.json';
@@ -9,10 +9,10 @@ import emailSuffixesInfo from '../data/emailSuffixes.json';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public version: string;
   title = 'signatureGenerator';
-  selectedLogo: { imgUrl: 'logo_img_ServicewareSE.PNG', name: 'Serviceware SE', alt: 'Serviceware SE' };
+  selectedLogo: { imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', name: 'Serviceware SE', alt: 'Serviceware SE' };
   firstName = '';
   lastName = '';
   position = '';
@@ -32,19 +32,10 @@ export class AppComponent {
   selectedLink = { text: '', underLinedText: '', url: '' };
 
   logos: [
-    { id: 0, name: 'Serviceware SE', imgUrl: 'logo_img_ServicewareSE.PNG', alt: 'Serviceware SE' },
-    { id: 1, name: 'PMCS - A SERVICEWARE COMPANY', imgUrl: 'logo_img_PMCS.PNG', alt: 'PMCS' },
-    { id: 2, name: 'SSC', imgUrl: 'logo_img_ssc.png', alt: 'Strategic Service Consulting' }
+    { id: 0, name: 'Serviceware SE', imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', alt: 'Serviceware SE' },
+    { id: 1, name: 'PMCS - A SERVICEWARE COMPANY', imgUrl: '../assets/images/logo_img_PMCS.PNG', alt: 'PMCS' },
+    { id: 2, name: 'SSC', imgUrl: '../assets/images/logo_img_ssc.png', alt: 'Strategic Service Consulting' }
   ];
-
-  // emailSuffixes: [
-  //   { id: 0, emailSuffix: 'Select Email' },
-  //   { id: 1, emailSuffix: '@anafee.com' },
-  //   { id: 2, emailSuffix: '@pmcs.de' },
-  //   { id: 3, emailSuffix: '@sabio.de' },
-  //   { id: 4, emailSuffix: '@serviceware.se' },
-  //   { id: 5, emailSuffix: '@strategic-sc.de' }
-  // ];
 
   public companies: any;
   public locations: any;
@@ -54,10 +45,15 @@ export class AppComponent {
     this.companies = comapnyInfo;
     this.locations = locationsInfo;
     this.emailSuffixes = emailSuffixesInfo;
-    console.log('companies data::', this.locations);
-  };
+  }
 
   ngOnInit(): void {
-    this.version = environment.version; // <-- Consume the version number from environment!
+    // this.version = environment.version; // <-- Consume the version number from environment!
+    this.version = '2.0.0';
+    this.selectedLogo = { imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', name: 'Serviceware SE', alt: 'Serviceware SE' };
+  }
+
+  submit(): void {
+
   }
 }
