557a71aba529d6d8de7792ce5851a2552ee5f103
colfin-96 <finger.colin@gmail.com>
2019-08-14 22:01:20 +0200

    -- broken -- changed a lot of stuff - please continue with adding two-way data binding to all fields and selects so that the preview can be generated --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../environments/environment';
 import comapnyInfo from '../data/companies.json';
 import locationsInfo from '../data/locations.json';
 import emailSuffixesInfo from '../data/emailSuffixes.json';
+import { CompleteCompany, CompleteLocation } from "../app/models/general.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public version: string;
-  title = 'signatureGenerator';
-  selectedLogo: { imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', name: 'Serviceware SE', alt: 'Serviceware SE' };
-  firstName = '';
-  lastName = '';
-  position = '';
-  completeLocation = { town: '', street: '', address1: '', address2: '', telephoneNum: '' };
-  completeCompany = { name: '', website: '', supportNum: '', imprints: [] };
-  emailPrefix = '';
-  finalTelephoneNum = '';
-  mobileNr = '';
-  showSupportNum = false;
-  langGer = false;
+    public version: string;
+    title = 'signatureGenerator';
+    selectedLogo: { imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', name: 'Serviceware SE', alt: 'Serviceware SE' };
+    firstName = '';
+    lastName = '';
+    position = '';
+    completeLocation: CompleteLocation = { town: '', street: '', address1: '', address2: '', telephoneNum: '', finalTelephoneNum: '' };
+    completeCompany: CompleteCompany = { name: '', website: '', supportNum: '', imprints: [] };
+    emailPrefix = '';
+    finalTelephoneNum = '';
+    mobileNr = '';
+    showSupportNum = false;
+    langGer = false;
 
-  currentTelNum = '';
+    currentTelNum = '';
 
-  showMobNum = true;
-  showTelNum = true;
+    showMobNum = true;
+    showTelNum = true;
 
-  selectedLink = { text: '', underLinedText: '', url: '' };
+    selectedLink = { text: '', underLinedText: '', url: '' };
 
-  logos: [
-    { id: 0, name: 'Serviceware SE', imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', alt: 'Serviceware SE' },
-    { id: 1, name: 'PMCS - A SERVICEWARE COMPANY', imgUrl: '../assets/images/logo_img_PMCS.PNG', alt: 'PMCS' },
-    { id: 2, name: 'SSC', imgUrl: '../assets/images/logo_img_ssc.png', alt: 'Strategic Service Consulting' }
-  ];
+    logos: [
+        { id: 0, name: 'Serviceware SE', imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', alt: 'Serviceware SE' },
+        { id: 1, name: 'PMCS - A SERVICEWARE COMPANY', imgUrl: '../assets/images/logo_img_PMCS.PNG', alt: 'PMCS' },
+        { id: 2, name: 'SSC', imgUrl: '../assets/images/logo_img_ssc.png', alt: 'Strategic Service Consulting' }
+    ];
 
-  public companies: any;
-  public locations: any;
-  public emailSuffixes: any;
+    public companies: any;
+    public locations: any;
+    public emailSuffixes: any;
 
-  public constructor() {
-    this.companies = comapnyInfo;
-    this.locations = locationsInfo;
-    this.emailSuffixes = emailSuffixesInfo;
-  }
+    public constructor() {
+        this.companies = comapnyInfo;
+        this.locations = locationsInfo;
+        this.emailSuffixes = emailSuffixesInfo;
+    }
 
-  ngOnInit(): void {
-    // this.version = environment.version; // <-- Consume the version number from environment!
-    this.version = '2.0.0';
-    this.selectedLogo = { imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', name: 'Serviceware SE', alt: 'Serviceware SE' };
-  }
+    ngOnInit(): void {
+        // this.version = environment.version; // <-- Consume the version number from environment!
+        this.version = '2.0.0';
+        this.selectedLogo = { imgUrl: '../assets/images/logo_img_ServicewareSE.PNG', name: 'Serviceware SE', alt: 'Serviceware SE' };
+    }
 
-  submit(): void {
+    getCompleteLocation(): CompleteLocation {
+        const completeLocation = this.completeLocation;
+        for (let i = 0; i < this.locations.locations.length; i++) {
+            if (this.locations.repeatSelect === i) {
+                completeLocation.street = this.locations.locations[i].street;
+                completeLocation.address1 = this.locations.locations[i].address1;
+                completeLocation.address2 = this.locations.locations[i].address2 !== '' ? this.locations.locations[i].address2 : '';
+                completeLocation.telephoneNum = this.locations.locations[i].telephoneNum;
+                completeLocation.town = this.locations.locations[i].town;
+            }
+        }
 
-  }
+        (document.getElementById('telNr') as HTMLInputElement).value = completeLocation.telephoneNum;
+        this.currentTelNum = completeLocation.telephoneNum;
+
+        if (this.finalTelephoneNum !== this.currentTelNum) {
+            completeLocation.finalTelephoneNum = this.finalTelephoneNum;
+        }
+
+        return completeLocation;
+    }
+
+    submit(): void {
+
+    }
 }
---
+++ src/app/models/general.model.ts
@@ -0,0 +1,15 @@
+export class CompleteLocation {
+    town: string;
+    street: string;
+    address1: string;
+    address2: string;
+    telephoneNum: string;
+    finalTelephoneNum: string;
+}
+
+export class CompleteCompany {
+    name: string;
+    website: string;
+    supportNum: string;
+    imprints: Array<string>;
+}
